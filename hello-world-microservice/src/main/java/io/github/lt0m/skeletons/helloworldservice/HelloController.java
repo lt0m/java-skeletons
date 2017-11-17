9c02637467a2726b62e491babd13dfeffe2f33e9
lt0m <lt0m@openmailbox.org>
2017-11-17 23:21:41 +0100

    Added Hello World docekrized microservice

---
+++ hello-world-microservice/src/main/java/io/github/lt0m/skeletons/helloworldservice/HelloController.java
@@ -0,0 +1,12 @@
+package io.github.lt0m.skeletons.helloworldservice;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    @RequestMapping("/sayhello")
+    public String sayHello() {
+        return "Hello";
+    }
+}
---
+++ hello-world-microservice/src/test/java/io/github/lt0m/skeletons/helloworldservice/HelloWorldServiceApplicationTest.java
@@ -0,0 +1,16 @@
+package io.github.lt0m.skeletons.helloworldservice;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class HelloWorldServiceApplicationTest {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
